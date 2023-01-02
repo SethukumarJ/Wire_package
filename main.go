af4706c875c97cf46b82cf5914381eee81078fa5
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-02 14:37:40 +0530

    Wire_package : had a good idea about wire package

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+   "fmt"
+)
+
+type Message string
+type Greeter struct {
+   Message Message
+}
+type Event struct {
+   Greeter Greeter
+}
+
+
+func GetMessage(text string) Message {
+   return Message(text)
+}
+func GetGreeter(m Message) Greeter {
+   return Greeter{Message: m}
+}
+func (g Greeter) Greet() Message {
+   return g.Message
+}
+func GetEvent(g Greeter) Event {
+   return Event{Greeter: g}
+}
+func (e Event) Start() {
+   msg := e.Greeter.Greet()
+   fmt.Println(msg)
+}
+func main() {
+	event := InitializeEvent("hello world")
+	event.Start()
+ }
